077f70f6f8b9d0d56d9b71e082bf1528e05d5479
Ахунов Денис <60143913+DeNyWho@users.noreply.github.com>
2023-09-28 23:12:35 +0300

    micro fix

--- src/main/kotlin/club/anifox/backend/service/auth/components/AuthComponent.kt
+++ src/main/kotlin/club/anifox/backend/service/auth/components/AuthComponent.kt
@@ -26,8 +26,6 @@ import org.keycloak.admin.client.resource.UserResource
 import org.keycloak.authorization.client.AuthzClient
 import org.keycloak.representations.idm.RoleRepresentation
 import org.keycloak.representations.idm.UserRepresentation
-import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.http.HttpStatus
 import org.springframework.security.authentication.BadCredentialsException
