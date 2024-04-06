146c70e8b2aa22911e66ff6459b90960be8ea851
Ахунов Денис <60143913+DeNyWho@users.noreply.github.com>
2024-04-06 20:14:12 +0300

    Add profiles

--- src/main/kotlin/club/anifox/backend/AnifoxBackendApplication.kt
+++ src/main/kotlin/club/anifox/backend/AnifoxBackendApplication.kt
@@ -1,34 +1,8 @@
 package club.anifox.backend
 
-import club.anifox.backend.service.anime.AnimeService
-import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.scheduling.annotation.EnableScheduling
-import org.springframework.stereotype.Component
-
-@Component
-class ScheduleTasks {
-
-    @Autowired
-    private lateinit var animeService: AnimeService
-
-//    @Scheduled(fixedDelay = 1, timeUnit = TimeUnit.HOURS)
-//    fun parseData() {
-//        animeService.parseTranslations(listOf(610, 609, 735, 643, 559, 739, 767, 825, 933, 557, 794, 1002, 1978, 1291, 1272, 1946))
-//        animeService.parseAnime()
-//    }
-
-//    @Scheduled(fixedDelay = 12, timeUnit = TimeUnit.HOURS)
-//    fun checkBlocked() {
-//        animeService.checkBlockedAnime()
-//    }
-
-//    @Scheduled(fixedDelay = 1, timeUnit = TimeUnit.HOURS)
-//    fun updateData() {
-//        animeService.updateEpisodes()
-//    }
-}
 
 @SpringBootApplication
 @EnableScheduling
---
+++ src/main/kotlin/club/anifox/backend/tasks/ParserTasks.kt
@@ -0,0 +1,27 @@
+package club.anifox.backend.tasks
+
+import club.anifox.backend.service.anime.AnimeService
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.context.annotation.Profile
+import org.springframework.scheduling.annotation.Scheduled
+import org.springframework.stereotype.Component
+import java.util.concurrent.TimeUnit
+
+@Component
+@Profile("parser")
+class ParserTasks {
+
+    @Autowired
+    private lateinit var animeService: AnimeService
+
+    @Scheduled(fixedDelay = 1, timeUnit = TimeUnit.HOURS)
+    fun parseData() {
+        animeService.parseTranslations(listOf(610, 609, 735, 643, 559, 739, 767, 825, 933, 557, 794, 1002, 1978, 1291, 1272, 1946))
+        animeService.parseAnime()
+    }
+
+    @Scheduled(fixedDelay = 1, timeUnit = TimeUnit.HOURS)
+    fun updateData() {
+        animeService.updateEpisodes()
+    }
+}
