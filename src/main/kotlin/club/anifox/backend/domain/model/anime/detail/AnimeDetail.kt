5ae355f8697f0bbb23c64a8ebda6e8f5333dfdd8
DeNyWho <denis.akhunov00@mail.ru>
2025-01-16 15:00:10 +0300

    🐛 Fixed anime detail request 🐛

--- src/main/kotlin/club/anifox/backend/domain/model/anime/detail/AnimeDetail.kt
+++ src/main/kotlin/club/anifox/backend/domain/model/anime/detail/AnimeDetail.kt
@@ -22,38 +22,38 @@ data class AnimeDetail(
     var title: String = "",
     var image: AnimeImages = AnimeImages(),
     @SerialName("player_link")
-    val playerLink: String? = null,
+    val playerLink: String? = "",
     val url: String = "",
-    val type: AnimeType? = null,
+    val type: AnimeType? = AnimeType.Tv,
     @SerialName("rating_mpa")
     val ratingMpa: String = "",
     @SerialName("minimal_age")
     val minimalAge: Int = 0,
-    val rating: Double? = null,
+    val rating: Double? = 0.0,
     @SerialName("shikimori_rating")
     val shikimoriRating: Double = 0.0,
     val year: Int = 0,
     val status: AnimeStatus = AnimeStatus.Ongoing,
     val season: AnimeSeason = AnimeSeason.Summer,
     @SerialName("episodes")
-    val episodes: Int? = null,
+    val episodes: Int? = 0,
     @SerialName("episodes_aired")
-    val episodesAired: Int? = null,
+    val episodesAired: Int? = 0,
     @SerialName("next_episode_on")
-    val nextEpisode: LocalDateTime? = null,
+    val nextEpisode: LocalDateTime? = LocalDateTime.now(),
     @SerialName("released_on")
-    val releasedOn: LocalDate? = null,
+    val releasedOn: LocalDate? = LocalDate.now(),
     @SerialName("aired_on")
     val airedOn: LocalDate = LocalDate.now(),
-    val description: String? = null,
+    val description: String? = "",
     @SerialName("other_title")
-    val titleOther: List<String>? = null,
+    val titleOther: List<String>? = listOf(),
     @SerialName("english")
-    val titleEnglish: List<String>? = null,
+    val titleEnglish: List<String>? = listOf(),
     @SerialName("japanese")
-    val titleJapan: List<String>? = null,
-    val synonyms: List<String> = listOf(),
+    val titleJapan: List<String>? = listOf(),
+    val synonyms: List<String>? = listOf(),
     val studio: List<AnimeStudio> = listOf(),
     val genres: List<AnimeGenre> = listOf(),
-    val translations: List<AnimeTranslation>? = null,
+    val translations: List<AnimeTranslation>? = listOf(),
 )
